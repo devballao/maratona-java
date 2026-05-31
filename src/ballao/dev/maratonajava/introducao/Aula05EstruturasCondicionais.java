7cf95f5fcc9b3afcc1e8818c8ecc5c76198872b9
Ballao.Dev <mateusrduarte@gmail.com>
2026-05-30 22:08:49 -0300

    update Aula05EstruturasCondicionais class to change main method visibility and add switch case examples

--- src/ballao/dev/maratonajava/introducao/Aula05EstruturasCondicionais.java
+++ src/ballao/dev/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -1,7 +1,7 @@
 package ballao.dev.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionais {
-    static void main(String[] args) {
+    public static void main(String[] args) {
         int idade = 17;
         boolean isAutorizadoComprarBebidaAlcoolica = idade >= 18;
         if (isAutorizadoComprarBebidaAlcoolica) {
@@ -42,5 +42,55 @@ public class Aula05EstruturasCondicionais {
             valorImposto = salarioAtual * TerceiraFaixa;
         }
         System.out.println("Valor do imposto: " + valorImposto);
+
+        // Switch
+        // Imprimir o dia da semana considerando o domingo como dia 1
+        byte dia = 5;
+        switch (dia) {
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda faixa");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta-feira");
+                break;
+            case 5:
+                System.out.println("Quinta-feira");
+                break;
+            case 6:
+                System.out.println("Sexta-feira");
+                break;
+            case 7:
+                System.out.println("Sábado");
+                break;
+            default:
+                System.out.println("Dia inválido");
+        }
+
+        // Exercicio com switch
+        /*
+        * Dado os valores de 1 a 7, improma se é dia últio ou final de semana
+        * considerando 1 como  domingo
+        */
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+            default:
+                System.out.println("Dia inválido");
+        }
     }
 }
