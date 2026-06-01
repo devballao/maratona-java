f9ea19f861120627ba82e45828b9435f32aa063f
Ballao.Dev <mateusrduarte@gmail.com>
2026-06-01 15:03:37 -0300

    add Estudante and ImpressoraEstudante classes with basic attributes and printing functionality

---
+++ src/ballao/dev/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,10 @@
+package ballao.dev.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprimir(Estudante estudante) {
+        System.out.println("---------------");
+        System.out.println("Nome: " + estudante.nome);
+        System.out.println("Idade: " + estudante.idade);
+        System.out.println("Sexo: " + estudante.sexo);
+    }
+}
---
+++ src/ballao/dev/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest.java
@@ -0,0 +1,23 @@
+package ballao.dev.maratonajava.javacore.Bintroducaometodos.test;
+
+import ballao.dev.maratonajava.javacore.Bintroducaometodos.dominio.Estudante;
+import ballao.dev.maratonajava.javacore.Bintroducaometodos.dominio.ImpressoraEstudante;
+
+public class EstudanteTest {
+    public static void main(String[] args) {
+        Estudante estudante01 = new Estudante();
+        Estudante estudante02 = new Estudante();
+        ImpressoraEstudante impressora = new ImpressoraEstudante();
+
+        estudante01.nome = "Midoriya";
+        estudante01.idade = 15;
+        estudante01.sexo = 'M';
+
+        estudante02.nome = "Sakura";
+        estudante02.idade = 16;
+        estudante02.sexo = 'F';
+
+        impressora.imprimir(estudante01);
+        impressora.imprimir(estudante02);
+    }
+}
