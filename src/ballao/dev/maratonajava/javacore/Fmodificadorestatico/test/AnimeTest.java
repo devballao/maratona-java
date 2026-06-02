7baee83af2333b9a5471b957a1c27a43bea4be18
Ballao.Dev <mateusrduarte@gmail.com>
2026-06-02 10:47:59 -0300

    add Anime class with static initialization block and methods; create AnimeTest for demonstration

---
+++ src/ballao/dev/maratonajava/javacore/Fmodificadorestatico/test/AnimeTest.java
@@ -0,0 +1,15 @@
+package ballao.dev.maratonajava.javacore.Fmodificadorestatico.test;
+
+import ballao.dev.maratonajava.javacore.Fmodificadorestatico.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime1 = new Anime();
+        Anime anime2 = new Anime();
+        Anime anime3 = new Anime();
+
+        System.out.println(anime1);
+        System.out.println(anime2);
+        System.out.println(anime3);
+    }
+}
