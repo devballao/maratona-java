342bedb9f7fcf351588749c92f61af5aa711ac66
Ballao.Dev <mateusrduarte@gmail.com>
2026-06-05 10:33:54 -0300

    add Jogador class with attributes and methods; create JogadorTeste for demonstration

---
+++ src/ballao/dev/maratonajava/javacore/Gassociacao/test/JogadorTeste.java
@@ -0,0 +1,16 @@
+package ballao.dev.maratonajava.javacore.Gassociacao.test;
+
+import ballao.dev.maratonajava.javacore.Gassociacao.dominio.Jogador;
+
+public class JogadorTeste {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Cafu");
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+        for (Jogador jogador : jogadores) {
+            jogador.imprimir();
+        }
+    }
+}
