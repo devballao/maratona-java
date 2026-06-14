074e795dbe59b8ddcacbc96cb5c62d29abcb2351
Ballao.Dev <mateusrduarte@gmail.com>
2026-06-14 20:50:59 -0300

    add TipoPagamento to Cliente class; update ClienteTest for demonstration

--- src/ballao/dev/maratonajava/javacore/Jenum/dominio/TipoCliente.java
+++ src/ballao/dev/maratonajava/javacore/Jenum/dominio/TipoCliente.java
@@ -1,6 +1,14 @@
 package ballao.dev.maratonajava.javacore.Jenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA,
-    PESSOA_JURIDICA
+    PESSOA_FISICA(1, "Pessoa Fisica"),
+    PESSOA_JURIDICA(2, "Pessoa Juridica");
+
+    public final int VALOR;
+    public final String NOME_RELATORIO;
+
+    TipoCliente(int valor,  String nomeRelatorio) {
+        this.VALOR = valor;
+        this.NOME_RELATORIO = nomeRelatorio;
+    }
 }
--- src/ballao/dev/maratonajava/javacore/Jenum/test/ClienteTest.java
+++ src/ballao/dev/maratonajava/javacore/Jenum/test/ClienteTest.java
@@ -5,14 +5,10 @@ import ballao.dev.maratonajava.javacore.Jenum.dominio.TipoCliente;
 
 public class ClienteTest {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Kuririn", TipoCliente.PESSOA_JURIDICA);
-        Cliente cliente3 = new Cliente("Jyraia", TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Zoro", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira", TipoCliente.PESSOA_JURIDICA,  Cliente.TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
     }
 }
