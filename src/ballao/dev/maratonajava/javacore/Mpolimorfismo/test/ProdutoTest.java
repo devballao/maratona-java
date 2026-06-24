53d494d19d428c0c55e751bdec8214be1ffe1db5
Ballao.Dev <mateusrduarte@gmail.com>
2026-06-23 23:11:20 -0300

    add data validity feature to Tomate class and demonstrate polymorphism in ProdutoTest

--- src/ballao/dev/maratonajava/javacore/Mpolimorfismo/test/ProdutoTest.java
+++ src/ballao/dev/maratonajava/javacore/Mpolimorfismo/test/ProdutoTest.java
@@ -1,6 +1,7 @@
 package ballao.dev.maratonajava.javacore.Mpolimorfismo.test;
 
 import ballao.dev.maratonajava.javacore.Mpolimorfismo.dominio.Computador;
+import ballao.dev.maratonajava.javacore.Mpolimorfismo.dominio.Produto;
 import ballao.dev.maratonajava.javacore.Mpolimorfismo.dominio.Tomate;
 import ballao.dev.maratonajava.javacore.Mpolimorfismo.servico.CalculadoraImposto;
 
@@ -12,5 +13,16 @@ public class ProdutoTest {
         CalculadoraImposto.calcularImpostoComputador(computador);
         System.out.println("---------------------");
         CalculadoraImposto.calcularImpostoTomate(tomate);
+
+        System.out.println("---------------------");
+        System.out.println("Polimorfismo");
+        Produto produto = new Computador("Ryzen 9", 3000.0);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println("---------------------");
+
+        Produto produto2 = new Tomate("Tomate Siciliano", 10.0);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
     }
 }
