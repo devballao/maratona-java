d5ddda9fe93a8bf6229dc9ac473a79ae6b9b8b66
Ballao.Dev <mateusrduarte@gmail.com>
2026-06-25 16:54:58 -0300

    rename RumtimeExeptionTest class to RumtimeExeptionTest01 and add RuntimeExeptioTest02 class to demonstrate exception handling in division

--- src/ballao/dev/maratonajava/javacore/Nexecoes/runtime/test/RumtimeExeptionTest01.java
+++ src/ballao/dev/maratonajava/javacore/Nexecoes/runtime/test/RumtimeExeptionTest01.java
@@ -1,6 +1,6 @@
 package ballao.dev.maratonajava.javacore.Nexecoes.runtime.test;
 
-public class RumtimeExeptionTest {
+public class RumtimeExeptionTest01 {
     public static void main(String[] args) {
         // Checked and Unchecked
         Object object = null;
---
+++ src/ballao/dev/maratonajava/javacore/Nexecoes/runtime/test/RuntimeExeptioTest02.java
@@ -0,0 +1,25 @@
+package ballao.dev.maratonajava.javacore.Nexecoes.runtime.test;
+
+public class RuntimeExeptioTest02 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(divisao(10, 0));
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @param num1
+     * @param num2 divisor não pode ser 0 (zero)
+     * @return num1 / num2
+     * @throws IllegalArgumentException se o divisor for 0 (zero)
+     */
+    public static int divisao(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Não é possível dividir por zero");
+        }
+        return num1 / num2;
+    }
+}
