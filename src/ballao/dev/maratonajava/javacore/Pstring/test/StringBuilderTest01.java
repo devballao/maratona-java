fc695621e518af95fb4436c9f60cfb29a17e6d09
Ballao.Dev <mateusrduarte@gmail.com>
2026-07-06 20:38:09 -0300

    add StringBuilderTest01 class to demonstrate StringBuilder usage and string manipulation in Java

---
+++ src/ballao/dev/maratonajava/javacore/Pstring/test/StringBuilderTest01.java
@@ -0,0 +1,14 @@
+package ballao.dev.maratonajava.javacore.Pstring.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "William Suane";
+        nome.concat(" DevDojo");
+        System.out.println(nome);
+
+        StringBuilder sb =  new StringBuilder("William Suane");
+        sb.append(" DevDojo").append(" Academy");
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
