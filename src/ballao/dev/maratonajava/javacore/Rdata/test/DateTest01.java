194ba6f8c18ca52172574920c7fd56b993ab0c2d
Ballao.Dev <mateusrduarte@gmail.com>
2026-07-07 21:20:16 -0300

    add DateTest01 class to demonstrate date manipulation in Java

---
+++ src/ballao/dev/maratonajava/javacore/Rdata/test/DateTest01.java
@@ -0,0 +1,13 @@
+package ballao.dev.maratonajava.javacore.Rdata.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        date.setTime(date.getTime() + 3_600_000L);
+        System.out.println(date);
+    }
+
+
+}
