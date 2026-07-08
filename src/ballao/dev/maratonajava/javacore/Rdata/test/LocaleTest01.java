6a1462c74879f153957cb22cbeee582f991f7384
Ballao.Dev <mateusrduarte@gmail.com>
2026-07-07 22:56:56 -0300

    add LocaleTest01 class to demonstrate date formatting across different locales in Java

---
+++ src/ballao/dev/maratonajava/javacore/Rdata/test/LocaleTest01.java
@@ -0,0 +1,35 @@
+package ballao.dev.maratonajava.javacore.Rdata.test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocaleTest01 {
+    public static void main(String[] args) {
+        Locale localeItaly = new Locale("it", "IT");
+        Locale localeSwitzerland = new Locale("de", "CH");
+        Locale localeBrazil = new Locale("pt", "BR");
+        Locale localeFrance = new Locale("fr", "FR");
+        Locale localeChina = new Locale("zh", "CN");
+        Locale localeJapan =  new Locale("ja", "JP");
+
+        Calendar calendar = Calendar.getInstance();
+        DateFormat dateFormat1 = DateFormat.getDateInstance(DateFormat.FULL, localeItaly);
+        DateFormat dateFormat2 = DateFormat.getDateInstance(DateFormat.FULL, localeSwitzerland);
+        DateFormat dateFormat3 = DateFormat.getDateInstance(DateFormat.FULL, localeBrazil);
+        DateFormat dateFormat4 = DateFormat.getDateInstance(DateFormat.FULL, localeFrance);
+        DateFormat dateFormat5 = DateFormat.getDateInstance(DateFormat.FULL, localeChina);
+        DateFormat dateFormat6 = DateFormat.getDateInstance(DateFormat.FULL, localeJapan);
+
+        System.out.println("Italy: " + dateFormat1.format(calendar.getTime()));
+        System.out.println("Switzerland: " + dateFormat2.format(calendar.getTime()));
+        System.out.println("Brazil: " + dateFormat3.format(calendar.getTime()));
+        System.out.println("France: " + dateFormat4.format(calendar.getTime()));
+        System.out.println("China: " + dateFormat5.format(calendar.getTime()));
+        System.out.println("Japan: " + dateFormat6.format(calendar.getTime()));
+
+        System.out.println(localeBrazil.getDisplayCountry(localeJapan));
+        System.out.println(localeBrazil.getDisplayCountry(localeItaly));
+        System.out.println(localeBrazil.getDisplayCountry(localeSwitzerland));
+    }
+}
