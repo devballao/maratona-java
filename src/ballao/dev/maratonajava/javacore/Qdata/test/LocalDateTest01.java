fa90b2612828c6fcbaf2b6aae5397c0382eba4f7
Ballao.Dev <mateusrduarte@gmail.com>
2026-07-08 22:03:47 -0300

    add LocalDateTest01 class to demonstrate usage of LocalDate in Java

---
+++ src/ballao/dev/maratonajava/javacore/Qdata/test/LocalDateTest01.java
@@ -0,0 +1,22 @@
+package ballao.dev.maratonajava.javacore.Qdata.test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+        System.out.println(localDate.getYear());
+        System.out.println(localDate.getMonth());
+        System.out.println(localDate.getMonthValue());
+        System.out.println(localDate.getDayOfWeek());
+        System.out.println(localDate.getDayOfMonth());
+        System.out.println(localDate.lengthOfMonth());
+    }
+}
